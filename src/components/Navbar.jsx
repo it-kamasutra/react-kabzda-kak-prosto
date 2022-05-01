f2608d6b39b33b97232613dc2a6933630615201c
Den denowich <denowich@gmail.com>
2022-05-01 18:03:39 +0300

    Update Navbar.jsx
    add className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return <nav className='nav'>
-        <div>
+        <div className='item'>
             <a>Profile</a>
         </div>
         <div>
