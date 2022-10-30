9e1d55f21c6bb6341c79cdb5879d33ff89e43d3c
Muhammadzhon <hmuhammadjon@gmail.com>
2022-10-30 23:23:31 +0600

    added an alt attribute for img tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return <header className='header'>
-        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+        <img alt='Ocean' src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
     </header>
 }
 
